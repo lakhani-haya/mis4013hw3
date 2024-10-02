f692d1050f5303bdbb9814d90e1956c5f50fb90e
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-02 12:12:22 -0500

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -9,14 +9,22 @@ while ($instructor = $instructors->fetch.assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $instructor['instructor_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
   <?php
   $courses = selectCoursesByInstructor($instructor['instructor_id']);
   while ($course = $courses->fetch_assoc()) {
 ?>
-    
+    <li class="list-group-item"><?php echo $course['course_number']; ?> - <?php echo $course['semester']; ?> - <?php echo $course['room']; ?> - <?php echo $course['day_time']; ?> </li>
+      <td><?php echo $course['course_id']; ?></td>
+  <td><?php echo $course['course_number']; ?></td>
+  <td><?php echo $course['course_description']; ?></td>
+  <td><?php echo $course['semester']; ?></td>
+  <td><?php echo $course['room']; ?></td>
+  <td><?php echo $course['day_time']; ?></td>
 <?php    
   }
   ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor['office_number']; ?></small></p>
     </div>
