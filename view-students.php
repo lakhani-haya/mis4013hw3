a4e5bbf0fe9726bd827f22ce35a46a8f9c497a78
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-02 12:32:06 -0500

    Update view-students.php

--- view-students.php
+++ view-students.php
@@ -3,7 +3,7 @@
   <table class="table">
    <thead>
      <tr>
-     <th>ID</th>
+      <th>ID</th>
      <th>Name</th>
      <th>Major</th>
      <th>Course</th>
@@ -13,19 +13,13 @@
    </thead>
    <tbody>
 <?php
-while ($course = $courses->fetch.assoc()) {
+while ($student = $students->fetch.assoc()) {
 ?>
 <tr>
   <td><?php echo $student['student_id']; ?></td>
   <td><?php echo $student['student_name']; ?></td>
   <td><?php echo $student['student_major']; ?></td>
   <td><?php echo $student['course_id']; ?></td>
-  <td>
-    <form method="post" action="sections-by-course.php">
-      <input type="hidden" name="cid" value="<?php echo $course['course_id']; ?>">
-     <button type="submit" class="btn btn-primary">Sections</button>
-    </form>
-  </td>
 </tr>
 <?php
 }
