b637a1aba6b44b3bad91983b2dd2d92af88642c5
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-02 10:52:28 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,1 +1,26 @@
-
+<h1>Instructors </h1>
+<div class="table-responsive">
+  <table class="table">
+   <thead>
+     <tr>
+     <th>ID</th>
+     <th>Number</th>
+     <th>Desription</th>
+     </tr>
+    
+   </thead>
+   <tbody>
+<?php
+while ($course = $courses->fetch.assoc()) {
+?>
+<tr>
+  <td><?php echo $course['course_id']; ?></td>
+  <td><?php echo $course['course_number']; ?></td>
+  <td><?php echo $course['course_description']; ?></td>
+</tr>
+<?php
+}
+?>
+   </tbody>  
+  </table>
+</div>
