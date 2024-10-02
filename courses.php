7ab557f5e2984112480198efade0268cc9ba65f6
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-02 11:01:20 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -1,15 +1,10 @@
 <?php
-function selectCourses {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM `course` ");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
+require_once("util-db.php");
+require_once("model-courses.php");
+
+$pageTitle = "Courses";
+Include "view-header.php";
+$instructors = selectCourses();
+include "view-courses.php";
+Include "view-footer.php";
 ?>
