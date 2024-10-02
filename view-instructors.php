a07326c553c27cd72013f2f90efb3ae5df7fcd96
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-02 10:56:35 -0500

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -6,6 +6,7 @@
      <th>ID</th>
      <th>Name</th>
      <th>Office</th>
+     <th></th>  
      </tr>
     
    </thead>
@@ -17,6 +18,7 @@ while ($instructor = $instructors->fetch.assoc()) {
   <td><?php echo $instructor['instructor_id']; ?></td>
   <td><?php echo $instructor['instructor_name']; ?></td>
   <td><?php echo $instructor['office_number']; ?></td>
+  <td><a href="courses-by-instructor.php?id=<?php echo $instructor['instructor_id']; ?>">Courses</a></td>
 </tr>
 <?php
 }
