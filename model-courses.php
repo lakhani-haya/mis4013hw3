5c2dd9798ce7c10f093ee0220f5c63bf0a51bc6c
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-14 13:34:14 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,5 +1,5 @@
 <?php
-function selectCourses {
+function selectCourses() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM `course` ");
