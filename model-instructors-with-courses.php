f1dc5a3f1b7e162186fade68d3276090bf5c9d68
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-26 13:17:49 -0500

    Update model-instructors-with-courses.php

--- model-instructors-with-courses.php
+++ model-instructors-with-courses.php
@@ -2,7 +2,7 @@
 function selectInstructors() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT instructor_id, instructor_name, office_number FROM `instructor` ");
+        $stmt = $conn->prepare("SELECT instructor_id, instructor_name, office_number FROM `instructor`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -16,7 +16,7 @@ function selectInstructors() {
 function selectCoursesByInstructors($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_id = c.course_id where s.instructor_id =? ");
+        $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c JOIN section s ON s.course_id = c.course_id WHERE s.instructor_id = ?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -28,3 +28,4 @@ function selectCoursesByInstructors($iid) {
     }
 }
 ?>
+
