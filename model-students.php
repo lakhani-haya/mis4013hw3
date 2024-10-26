c00f8e560fcb2fafc5274e424cd41b889916d5f7
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-26 13:24:55 -0500

    Update model-students.php

--- model-students.php
+++ model-students.php
@@ -1,8 +1,8 @@
 <?php
-function selectStudents {
+function selectStudents() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select student_id, student_name, student_major, course_id from student");
+        $stmt = $conn->prepare("SELECT student_id, student_name, student_major, course_id FROM student");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
