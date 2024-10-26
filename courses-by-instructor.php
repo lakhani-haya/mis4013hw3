ed975e84d1309907273fcbad57a471c065c55038
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-26 13:03:09 -0500

    Update courses-by-instructor.php

--- courses-by-instructor.php
+++ courses-by-instructor.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-courses-by-instructor.php");
 
 $pageTitle = "Courses by Instructor";
-Include "view-header.php";
-$instructors = selectCoursesByInstructor($_GET['id']);
+include "view-header.php";
+$courses = selectCoursesByInstructors($_GET['id']);
 include "view-courses-by-instructor.php";
-Include "view-footer.php";
+include "view-footer.php";
 ?>
