cf6ea0404cb868ead748d2921d4e0cd4a4408051
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-26 12:56:16 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,17 +1,14 @@
+
 <?php
 function selectCourses() {
-    $conn = null; 
-    try {
-        $conn = get_db_connection(); // Get the database connection
-        $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM `course`");
-        $stmt->execute();
-        
-        $result = $stmt->get_result(); // Fetch the result
-        return $result;
-    } catch (Exception $e) {
-        if ($conn) {
-            $conn->close(); 
-
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("SELECT course_id, course_number, course_description FROM `course`");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $conn->close();
+    return $result;
 }
 ?>
+
+
 
