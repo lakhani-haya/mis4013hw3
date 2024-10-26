bfc371369f648ea65a6f62613bcbb6b6d4a2a47a
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-26 13:18:08 -0500

    Update instructors-with-courses.php

--- instructors-with-courses.php
+++ instructors-with-courses.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-instructors-with-courses.php");
 
 $pageTitle = "Instructors with Courses";
-Include "view-header.php";
+include "view-header.php";
 $instructors = selectInstructors();
 include "view-instructors-with-courses.php";
-Include "view-footer.php";
+include "view-footer.php";
 ?>
