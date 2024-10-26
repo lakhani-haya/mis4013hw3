fcb7ccaf4e1a59bd9bf6e2d5a83c3968ca52623c
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-10-26 13:42:57 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,9 +1,9 @@
 <?php
 function get_db_connection(){
-    // Create connection
+  
     $conn = new mysqli('138.197.17.168', 'hwhayaou_hw3user', 'hihelloSQL1', 'hwhayaou_hw3');
     
-    // Check connection
+  
     if ($conn->connect_error) {
       return false;
     }
